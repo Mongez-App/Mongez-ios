7eb4cf922335dde6b5cbaae81f9b8040ecce1db9
soutAhmedTayseer <shadyramadan.sr@gmail.com>
2026-07-15 16:23:20 -0700

    AppTheme initial structure

---
+++ Modules/Common/Sources/Common/Themes/AppTheme.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  Common
+//
+//  Created by shady ramadan on 15/07/2026.
+//
+
+import Foundation
+
+public enum AppTheme{
+    
+    public enum Colors {
+   
+    }
+    public enum Typography{
+        
+    }
+    public enum Spacing{
+        
+    }
+}
