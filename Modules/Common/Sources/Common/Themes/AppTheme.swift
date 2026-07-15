042ec7af155e6ce627263668f6cff5638122e475
Mazen Amr <139044003+mazen675@users.noreply.github.com>
2026-07-15 16:46:58 +0300

    added spacing

--- Modules/Common/Sources/Common/Themes/AppTheme.swift
+++ Modules/Common/Sources/Common/Themes/AppTheme.swift
@@ -35,6 +35,12 @@ public enum AppTheme{
         
     }
     public enum Spacing{
-        
+        public static let xxSmall: CGFloat = 2
+        public static let xSmall: CGFloat  = 4
+        public static let small: CGFloat   = 8
+        public static let medium: CGFloat  = 16
+        public static let large: CGFloat   = 24
+        public static let xLarge: CGFloat  = 32
+        public static let xxLarge: CGFloat = 48
     }
 }
