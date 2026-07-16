69c403904e0136c1f5a9624baf7f564d4b724eb8
soutAhmedTayseer <shadyramadan.sr@gmail.com>
2026-07-15 18:12:36 -0700

    finalize App Theme Colors

--- Modules/Common/Sources/Common/Themes/AppTheme.swift
+++ Modules/Common/Sources/Common/Themes/AppTheme.swift
@@ -12,16 +12,16 @@ public enum AppTheme{
     
     public enum Colors {
         public static var primaryColor : Color {
-            Color("#4B4DED")
+            Color(hex: "#4B4DED")
         }
         public static var backGround : Color{
-            Color("#F9F9FF")
+            Color(hex: "#F9F9FF")
         }
         public static var primaryText : Color{
-            Color("#111827")
+            Color(hex: "#111827")
         }
         public static var secondoryText : Color{
-            Color("#6B7280")
+            Color(hex: "#6B7280")
         }
       
         public static var BtnText : Color{
@@ -42,5 +42,29 @@ public enum AppTheme{
         public static let large: CGFloat   = 24
         public static let xLarge: CGFloat  = 32
         public static let xxLarge: CGFloat = 48
+    }
+    
+}
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let r, g, b: UInt64
+        switch hex.count {
+        case 3:
+            (r, g, b) = ((int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6:
+            (r, g, b) = (int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (r, g, b) = (1, 1, 1)
+        }
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: 1
+        )
     }
 }
