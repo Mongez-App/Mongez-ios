936431c826b820ad27988340cc9970afa786d1b3
Mazen Amr <139044003+mazen675@users.noreply.github.com>
2026-07-30 23:44:25 +0300

    chore: Update core dependency injection

---
+++ Modules/Common/Sources/Common/DI/DIAssembly.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mazen Amr on 24/07/2026.
+//
+
+import Foundation
+import Swinject
+
+public protocol DIAssembly {
+    func assemble(container: Container)
+}
---
+++ Modules/Common/Sources/Common/DI/DIContainer.swift
@@ -0,0 +1,66 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mazen Amr on 24/07/2026.
+//
+
+import Foundation
+import Swinject
+
+public class DIContainer {
+    public static let shared = DIContainer()
+    
+    private let container: Container
+    
+    private init() {
+        self.container = Container()
+      //  registerDefaultServices()
+    }
+
+//    private func registerDefaultServices() {
+//        
+//    }
+    
+    public func register<Service>(
+        _ serviceType: Service.Type,
+        factory: @escaping (Resolver) -> Service
+    ) {
+        container.register(serviceType) { factory($0) }
+    }
+    
+    public func register<Service, Arg1, Arg2>(
+        _ serviceType: Service.Type,
+        factory: @escaping (Resolver, Arg1, Arg2) -> Service
+    ) {
+        container.register(serviceType) { factory($0, $1, $2) }
+    }
+    
+    public func register<Service>(
+        _ serviceType: Service.Type,
+        name: String?,
+        factory: @escaping (Resolver) -> Service
+    ) {
+        container.register(serviceType, name: name) { factory($0) }
+    }
+    
+    public func resolve<Service>(_ serviceType: Service.Type) -> Service? {
+        return container.resolve(serviceType)
+    }
+    
+    public func resolve<Service, Arg1, Arg2>(
+        _ serviceType: Service.Type,
+        arguments arg1: Arg1,
+        _ arg2: Arg2
+    ) -> Service? {
+        return container.resolve(serviceType, arguments: arg1, arg2)
+    }
+    
+    public func resolve<Service>(_ serviceType: Service.Type, name: String?) -> Service? {
+        return container.resolve(serviceType, name: name)
+    }
+    
+    public func getContainer() -> Container {
+        return container
+    }
+}
