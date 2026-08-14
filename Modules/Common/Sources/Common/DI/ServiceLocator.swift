1c79461849c32f21395f09d6db6f0945feeefe0f
Mazen Amr <139044003+mazen675@users.noreply.github.com>
2026-08-14 22:36:46 +0300

    feat(Common) : add extra resolve method to the service locator

--- Modules/Common/Sources/Common/DI/ServiceLocator.swift
+++ Modules/Common/Sources/Common/DI/ServiceLocator.swift
@@ -6,10 +6,19 @@
 //
 
 import Foundation
+import Swinject
 
 public class ServiceLocator {
+    
     public static func resolve<Service>(_ serviceType: Service.Type) -> Service? {
         return DIContainer.shared.resolve(serviceType)
+    }
+    
+    public static func resolve<Service, Arg1>(
+        _ serviceType: Service.Type,
+        arguments arg1: Arg1
+    ) -> Service? {
+        return DIContainer.shared.resolve(serviceType, argument: arg1)
     }
     
     public static func resolve<Service, Arg1, Arg2>(
